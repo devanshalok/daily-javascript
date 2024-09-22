9d00f16a58f26122920c636f0a33e0984a2cf947
Devansh Alok <devanshalok@gmail.com>
2024-09-22 12:17:09 -0700

    new concepts

--- index.js
+++ index.js
@@ -65,4 +65,79 @@ console.log(mul)
 console.log(mul[0][0]=='1') //equality (it converts the type)
 console.log(mul[0][0]==='1') //strict equality (doesnot convert the type)
 
-console.log("hello")
+let myPlants=[{type:"flowers",list:['rose','tulip','yukon']}]
+
+console.log(myPlants[0].list)
+
+//Loops
+
+let myArray =[]
+let i=0
+
+while (i<5){
+    myArray.push(i)
+    i++
+}
+console.log(myArray)
+
+for(let j=0;j<10;j++){
+    myArray.push(j)
+}
+console.log(myArray)
+
+for (let z=10;z>0;z-=2){
+    myArray.push(z)
+}
+console.log(myArray)
+
+let prod=[[1,2,3],[5,6],[7,8,9,10]]
+let product=1
+
+for (let z=0;z<prod.length;z++){
+    for (let t=0;t<prod[z].length;t++){
+        product*=prod[z][t]
+    }
+}
+console.log(product)
+
+function convertToInteger(str){
+    return parseInt(str)
+}
+d=convertToInteger("90")
+console.log(d)
+
+
+//ternary operator
+let r=-987
+console.log(r<0? 'postive': 'negative')
+
+//arrow functions
+
+magic = () => 2+3;
+console.log(magic())
+
+const myConcat=(arr1,arr2)=>arr1.concat(arr2)
+
+console.log(myConcat([12,3,3],[6,7,8]))
+
+const realNumberArray =[-9,8.89,7,2,3.6]
+
+const sqaureList=(arr)=>{
+    const squaredIntegers=arr.filter(num=>Number.isInteger(num) && num>0).map(x=>x*x)
+    return squaredIntegers
+}
+console.log(sqaureList(realNumberArray))
+
+//rest operator
+const sum =(...args)=>{
+    return args.concat(8)
+}
+
+console.log(sum(1,2,3))
+
+//spread operator
+arr=[...realNumberArray]
+realNumberArray[0]=9876
+
+console.log(arr)
+console.log(realNumberArray)
